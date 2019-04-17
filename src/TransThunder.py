8b2f94cb78d2b2f93be1ce1a2ddbb7b785a207f8
HSS241 <honshin24@yahoo.co.jp>
2019-04-17 20:59:03 +0900

    ソース上げミス

--- src/TransThunder.py
+++ src/TransThunder.py
@@ -8,7 +8,10 @@ import colorama
 from colorama import Back
 import win_unicode_console
 
-def LangChecker(lang):  #翻訳先の言語の設定
+lang = 'en'
+battle = False
+def LangChecker():  #翻訳先の言語の設定
+    global lang
     if (os.path.isfile('.\\TransThunder.txt')):
         with open ('.\\TransThunder.txt') as f:
             lang = f.readline()
@@ -16,71 +19,56 @@ def LangChecker(lang):  #翻訳先の言語の設定
     else:
         with open ('.\\TransThunder.txt', 'w') as f:
             f.write('lang:en')
-    return lang
+            lang = 'en'
 
-def MsgChecker(lastId, lang):   #翻訳するメッセージの選択
+def MsgChecker(lastId):   #翻訳するメッセージの選択
     res = []
-    try:
-        res = json.loads(requests.get('http://localhost:8111/gamechat?lastId=0').text)
-    except:
-        return res, lastId
-    if (len(res) == 0):
-        lastId = 0
-        return res, lastId
-        
-    try:
-        res = json.loads(requests.get('http://localhost:8111/gamechat?lastId=' + str(lastId)).text)
-    except:
-        return res, lastId
-    if (len(res) == 0):
-        return res, lastId
+    lastIdList = ['0', str(lastId)]
+    for parameter in lastIdList:
+        try:
+            res = json.loads(requests.get('http://localhost:8111/gamechat?lastId=' + parameter).text)
+        except:
+            return res, lastId
+        if (len(res) == 0):
+            if (parameter == '0'):
+                global battle
+                battle = False
+            return res, lastId
 
-    if (lastId == 0):
-            print ('------------------------------')
-            
     lastId = res[0]['id']
-    
     if (len(res) == 0 or re.search('<color=#\w*>', res[0]['msg']) or
         Translator().detect(res[0]['msg']).lang == lang or res[0]['sender'] == ''):
         res = []
-        
+
     return res, lastId
 
-def Main():
-    lang = 'en'
-    lastId = 0
+def MsgViewer (res):
+    msg = res[0]['msg']
+    msg = msg.replace('\t', '')
 
-    lang = LangChecker(lang)
+    forecolor = Back.CYAN
+    reset = Back.RESET
+    if (res[0]['enemy'] == True):
+        forecolor = Back.RED
+
+    global battle
+    if (battle is False):
+        print ('------------------------------')
+    print (forecolor + ':' + reset + ' ' + res[0]['sender'] + ' [' + res[0]['mode'] + ']')
+    print (msg)
+    print (Translator().translate(msg, dest = lang).text + '\r\n')
+    battle = True
+
+def Main():
+    LangChecker()
+    lastId = 0
     while (True):
         sleep(1)
-        
-        res, lastId = MsgChecker(lastId, lang)
+        res, lastId = MsgChecker(lastId)
         if (len(res) == 0):
             continue
-        
-        msg = res[0]['msg']
-        msg = msg.replace('\t', '')
 
-        forecolor = Back.CYAN
-        reset = Back.RESET
-        if (res[0]['enemy'] == True):
-            forecolor = Back.RED
-
-        try:
-            print (forecolor + ':' + reset + ' ' + res[0]['sender'] + ' [' + res[0]['mode'] + ']')
-        except:
-            print (forecolor + ':' + reset + ' ??? [' + res[0]['mode'] + ']')
-
-        try:
-            print (msg)
-        except:
-            print ('???')
-
-        try:
-            print (Translator().translate(msg, dest = lang).text + '\r\n')
-        except:
-            print ('???')
-
+        MsgViewer(res)
         
 if __name__ == '__main__':
     win_unicode_console.enable()
