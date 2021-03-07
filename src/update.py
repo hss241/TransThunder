a993d5d71d2d9cdc29e8ea6c6ecb58f4e27990a7
HSS241 <honshin24@yahoo.co.jp>
2021-03-07 21:18:12 +0900

    minor_fix

--- src/update.py
+++ src/update.py
@@ -7,7 +7,7 @@ import subprocess
 
 class Update():
     def __init__(self):
-        self.version = "v3.4.3"
+        self.version = "v3.4.4"
         os.system("title TransThunder " + self.version)
 
         self.dlurl = "https://github.com/hss241/TransThunder/blob/master/TransThunder.exe?raw=true"
